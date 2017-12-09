3abb816a76a28b521c86ac349ca09c2698e2d2bf
Jared Pleva <jpleva91@gmail.com>
2017-12-09 15:46:52 -0700

    implements faker and lodash into server/main.js

--- server/main.js
+++ server/main.js
@@ -1,6 +1,23 @@
 // Only executed on the server
+import _ from 'lodash';
 import { Meteor } from 'meteor/meteor';
+import { Employees } from '../imports/collections/employee';
+import { image, helpers } from 'faker';
 
 Meteor.startup(() => {
   // Great place to generate data
+
+  // Check to see if data exists in the collection
+  const numberRecords = Employees.find({}).count();
+  if (!numberRecords) {
+    // Generate data...
+    _.times(5000, () => {
+      const { name, email, phone } = helpers.createCard();
+
+      Employees.insert({
+        name, email, phone,
+        avatar: image.avatar()
+      });
+    });
+  }
 });
